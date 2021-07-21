c58900e8eae50af8793be8dead7105127774d08e
ravik <ravik@LAPTOP-LCJ2GCBE>
2021-07-21 20:34:12 -0300

    Adicionar arquivos de projeto.

---
+++ aula-4-2/VendedorContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_4_2
+{
+    class VendedorContext : DbContext
+    {
+        
+    }
+}
